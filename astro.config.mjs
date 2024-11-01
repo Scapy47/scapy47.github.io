11318d7eca762f91197d18cdf46733e71ca66821
scapy47 <scapy47>
2024-11-01 17:27:01 +0530

    Deployed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astronaut.github.io',
+    site: 'https://Scapy47.github.io',
     base: 'Maths',
 });
